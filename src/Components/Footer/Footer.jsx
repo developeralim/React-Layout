a9a534d966712c84b983d6c414ba46c56ee24c63
developeralim <developeralimcu@gmail.com>
2023-02-01 09:12:06 +0600

    All files as well as folders going to be commited

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './Footer.module.css';
+
+export default function Footer() {
+      return (
+            <h1 className={styles.test}>Footer</h1>
+      )
+}
